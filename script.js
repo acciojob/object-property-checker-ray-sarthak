5fd74c38ab9be8e6ce9b9dc9df29f89d949a7012
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-12 23:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(const keys in sampleObject){ 
+		if(key===keys){
+			return true;
+		}
+	}
+	return false;
 }
 
 // Do not change the code below
